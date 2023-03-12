2d7ef6895a8b7d99238e29627266750a2181bd81
Agam <iamragepie@gmail.com>
2023-03-12 23:45:26 +0200

    BitBoards - added 3 function removePieceOnSquare, movePieceFromCurrentToTargetSquare and movePiece. PieceMovement - added 2 function, executeMove and getMovesAsBitMap - uses removePieceOnSquare and movePieceFromCurrentToTargetSquare to do so tempGui - updated to use executeMove and getMoveAsBitMap

--- src/PieceMovement.java
+++ src/PieceMovement.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 public class PieceMovement {
 
     private final BitBoards bitBoards;
+    private static final BoardUtils utils = new BoardUtils();
 
     private static final long[] KING_MOVES = new long[BoardUtils.BOARD_SIZE];
     private static final HashMap<Long, Long>[] ROOK_MOVES = (HashMap<Long, Long>[]) new HashMap<?, ?>[BoardUtils.BOARD_SIZE];
@@ -16,7 +17,7 @@ public class PieceMovement {
     private static final long[] ROOK_MASK = new long[BoardUtils.BOARD_SIZE];
     private static final long[] BISHOP_MASK = new long[BoardUtils.BOARD_SIZE];
 
-    // builder receive bitboard, build all moves per square piece
+    // builder receive bitboard, precalculate all the moves a piece can do on each square and save it
     public PieceMovement(BitBoards bitBoards) {
         for (int i = 0; i < BoardUtils.BOARD_SIZE; i++) {
             ROOK_MOVES[i] = new HashMap<>();
@@ -33,21 +34,71 @@ public class PieceMovement {
         generator.generateMaskBishop(BISHOP_MASK);
     }
 
+    // Given the target square which the piece wants to go to, and the currentSquare of the piece that want to move
+    // Change the bitboards according to it
+    public void executeMove(byte currentSquare, byte targetSquare) {
+        bitBoards.removePieceOnSquare(targetSquare);
+        bitBoards.movePieceFromCurrentToTargetSquare(currentSquare,targetSquare);
+    }
+
+    // Given a square, return a long that represent a bit map of the moves it can do
+    public long getMovesAsBitMap(byte square) {
+        long bitPosition = utils.getSquarePositionAsBitboardPosition(square);
+        // Check which piece it is, and
+        if ((bitPosition & bitBoards.getWhiteKing()) != 0)
+            return getKingMovement(square, BoardUtils.WHITE);
+
+        if ((bitPosition & bitBoards.getWhiteQueens()) != 0)
+            return getQueenMovement(square, BoardUtils.WHITE);
+
+        if ((bitPosition & bitBoards.getWhiteRooks()) != 0)
+            return getRookMovement(square, BoardUtils.WHITE);
+
+        if ((bitPosition & bitBoards.getWhiteBishops()) != 0)
+            return getBishopMovement(square, BoardUtils.WHITE);
+
+        if ((bitPosition & bitBoards.getWhiteKnights()) != 0)
+            return getKnightMovement(square, BoardUtils.WHITE);
+
+        if ((bitPosition & bitBoards.getWhitePawns()) != 0)
+            return getPawnMovement(square, BoardUtils.WHITE);
+
+        if ((bitPosition & bitBoards.getBlackKing()) != 0)
+            return getKingMovement(square, BoardUtils.BLACK);
+
+        if ((bitPosition & bitBoards.getBlackQueens()) != 0)
+            return getQueenMovement(square, BoardUtils.BLACK);
+
+        if ((bitPosition & bitBoards.getBlackRooks()) != 0)
+            return getRookMovement(square, BoardUtils.BLACK);
+
+        if ((bitPosition & bitBoards.getBlackBishops()) != 0)
+            return getBishopMovement(square, BoardUtils.BLACK);
+
+        if ((bitPosition & bitBoards.getBlackKnights()) != 0)
+            return getKnightMovement(square, BoardUtils.BLACK);
+
+        if ((bitPosition & bitBoards.getBlackPawns()) != 0)
+            return getPawnMovement(square, BoardUtils.BLACK);
+
+        return 0;
+    }
+
     // Given a position of a king, return the moves it can do as bitboard
-    public long getKingMovement(byte position, boolean color) {
+    private long getKingMovement(byte position, boolean color) {
         // All the moves it can do without position occupied by same color pieces
         long moves = KING_MOVES[position];
         return moves & ~bitBoards.allPiecesWithSameColor(color);
     }
 
     // Given a position of a queen, return the moves it can do as bitboard
-    public long getQueenMovement(byte position, boolean color) {
+    private long getQueenMovement(byte position, boolean color) {
         // Queen moves like a rook and a bishop, and remove square with same color pieces
         return getRookMovement(position, color) | getBishopMovement(position, color) & ~bitBoards.allPiecesWithSameColor(color);
     }
 
     // Given a position of a rook, return the moves it can do as bitboard
-    public long getRookMovement(byte position, boolean color) {
+    private long getRookMovement(byte position, boolean color) {
         // Calculate the bitBoard & mask val - key to move for that position, and remove square with same color pieces
         long keyVal = ROOK_MASK[position] & bitBoards.getAllPieces();
         long moves = ROOK_MOVES[position].get(keyVal);
@@ -55,7 +106,7 @@ public class PieceMovement {
     }
 
     // Given a position of a bishop, return the moves it can do as bitboard
-    public long getBishopMovement(byte position, boolean color) {
+    private long getBishopMovement(byte position, boolean color) {
         // Calculate the bitBoard & mask val - key to move for that position, and remove square with same color pieces
         long keyVal = BISHOP_MASK[position] & bitBoards.getAllPieces();
         long moves = BISHOP_MOVES[position].get(keyVal);
@@ -63,14 +114,14 @@ public class PieceMovement {
     }
 
     // Given a position of a knight, return the moves it can do as bitboard
-    public long getKnightMovement(byte position, boolean color) {
+    private long getKnightMovement(byte position, boolean color) {
         // All the moves it can do without position occupied by same color pieces
         long moves = KNIGHT_MOVES[position];
         return moves & ~bitBoards.allPiecesWithSameColor(color);
     }
 
     // Given a position of a pawn, return the moves it can do as bitboard
-    public long getPawnMovement(byte position, boolean color) {
+    private long getPawnMovement(byte position, boolean color) {
         // pawn can only capture if there is an enemy piece, and move only if its empty
         if (color == BoardUtils.WHITE)
             return ((WHITE_PAWN_CAPTURE[position] & bitBoards.getBlackPieces())) | ((WHITE_PAWN_ONLY_MOVES[position] & ~bitBoards.getAllPieces()));
--- src/tempGui.java
+++ src/tempGui.java
@@ -104,9 +104,8 @@ public class tempGui extends JFrame {
     private void function(int currRow, int curCol) {
 
         if (preCol == -1 && preRow == -1) {
-            long square = (currRow * 8L) + curCol;
-            long bitSquare = 1L << square;
-            long movement = squareToMoves((byte) square, bitSquare);
+            byte square = (byte) ((currRow * 8) + curCol);
+            long movement = pieceMovement.getMovesAsBitMap(square);
             System.out.println("row: " + currRow + " col: " + curCol);
 
             if (movement != 0) {
@@ -124,10 +123,10 @@ public class tempGui extends JFrame {
             preMoves = movement;
         } else {
             if (preMoves != 0) {
-                int squareOffCurr = (currRow * 8) + curCol, preSquare = (preRow * 8) + preCol;
+                byte targetSquare = (byte) ((currRow * 8) + curCol), currentSquare = (byte) ((preRow * 8) + preCol);
                 boolean[] possibleMoves = convertLongMovementToArr(preMoves);
-                if (possibleMoves[squareOffCurr]) {
-                    executeMove(squareOffCurr, preSquare);
+                if (possibleMoves[targetSquare]) {
+                    pieceMovement.executeMove(currentSquare, targetSquare);
                 }
             }
 
@@ -135,54 +134,6 @@ public class tempGui extends JFrame {
             preRow = -1;
             updateBoard();
         }
-
-
-    }
-
-    private void executeMove(int squareOffCurr, int preSquare) {
-        long bitSquare = 1L << preSquare;
-
-        if ((bitSquare & bitBoards.getWhiteKing()) != 0)
-            bitBoards.setWhiteKing(changeVal(bitBoards.getWhiteKing(), bitSquare, squareOffCurr));
-
-        if ((bitSquare & bitBoards.getWhiteQueens()) != 0)
-            bitBoards.setWhiteQueens(changeVal(bitBoards.getWhiteQueens(), bitSquare, squareOffCurr));
-
-        if ((bitSquare & bitBoards.getWhiteRooks()) != 0)
-            bitBoards.setWhiteRooks(changeVal(bitBoards.getWhiteRooks(), bitSquare, squareOffCurr));
-
-        if ((bitSquare & bitBoards.getWhiteBishops()) != 0)
-            bitBoards.setWhiteBishops(changeVal(bitBoards.getWhiteBishops(), bitSquare, squareOffCurr));
-
-        if ((bitSquare & bitBoards.getWhiteKnights()) != 0)
-            bitBoards.setWhiteKnights(changeVal(bitBoards.getWhiteKnights(), bitSquare, squareOffCurr));
-
-        if ((bitSquare & bitBoards.getWhitePawns()) != 0)
-            bitBoards.setWhitePawns(changeVal(bitBoards.getWhitePawns(), bitSquare, squareOffCurr));
-
-        if ((bitSquare & bitBoards.getBlackKing()) != 0)
-            bitBoards.setBlackKing(changeVal(bitBoards.getBlackKing(), bitSquare, squareOffCurr));
-
-        if ((bitSquare & bitBoards.getBlackQueens()) != 0)
-            bitBoards.setBlackQueens(changeVal(bitBoards.getBlackQueens(), bitSquare, squareOffCurr));
-
-        if ((bitSquare & bitBoards.getBlackRooks()) != 0)
-            bitBoards.setBlackRooks(changeVal(bitBoards.getBlackRooks(), bitSquare, squareOffCurr));
-
-        if ((bitSquare & bitBoards.getBlackBishops()) != 0)
-            bitBoards.setBlackBishops(changeVal(bitBoards.getBlackBishops(), bitSquare, squareOffCurr));
-
-        if ((bitSquare & bitBoards.getBlackKnights()) != 0)
-            bitBoards.setBlackKnights(changeVal(bitBoards.getBlackKnights(), bitSquare, squareOffCurr));
-
-        if ((bitSquare & bitBoards.getBlackPawns()) != 0)
-            bitBoards.setBlackPawns(changeVal(bitBoards.getBlackPawns(), bitSquare, squareOffCurr));
-    }
-
-    private long changeVal(long bitBoardOfPiece, long bitSquare, int squareOffCurr) {
-        long currPiece = bitBoardOfPiece & ~bitSquare;
-        return currPiece | (1L << squareOffCurr);
-
     }
 
     private char squareToPiece(long square) {
@@ -224,46 +175,4 @@ public class tempGui extends JFrame {
 
         return 0;
     }
-
-    private long squareToMoves(byte square, long bitSquare) {
-        if ((bitSquare & bitBoards.getWhiteKing()) != 0)
-            return pieceMovement.getKingMovement(square, true);
-
-        if ((bitSquare & bitBoards.getWhiteQueens()) != 0)
-            return pieceMovement.getQueenMovement(square, true);
-
-        if ((bitSquare & bitBoards.getWhiteRooks()) != 0)
-            return pieceMovement.getRookMovement(square, true);
-
-        if ((bitSquare & bitBoards.getWhiteBishops()) != 0)
-            return pieceMovement.getBishopMovement(square, true);
-
-        if ((bitSquare & bitBoards.getWhiteKnights()) != 0)
-            return pieceMovement.getKnightMovement(square, true);
-
-        if ((bitSquare & bitBoards.getWhitePawns()) != 0)
-            return pieceMovement.getPawnMovement(square, true);
-
-        if ((bitSquare & bitBoards.getBlackKing()) != 0)
-            return pieceMovement.getKingMovement(square, false);
-
-        if ((bitSquare & bitBoards.getBlackQueens()) != 0)
-            return pieceMovement.getQueenMovement(square, false);
-
-        if ((bitSquare & bitBoards.getBlackRooks()) != 0)
-            return pieceMovement.getRookMovement(square, false);
-
-        if ((bitSquare & bitBoards.getBlackBishops()) != 0)
-            return pieceMovement.getBishopMovement(square, false);
-
-        if ((bitSquare & bitBoards.getBlackKnights()) != 0)
-            return pieceMovement.getKnightMovement(square, false);
-
-        if ((bitSquare & bitBoards.getBlackPawns()) != 0)
-            return pieceMovement.getPawnMovement(square, false);
-
-        return 0;
-    }
-
-
 }
