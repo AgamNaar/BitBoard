f180180e7be583e5412bab3e50be9d0cf1e020a2
swifft <iamragepie@gmail.com>
2023-05-05 17:49:36 +0300

    PieceMovement - print position as notion.

--- src/gameengine/PieceMove.java
+++ src/gameengine/PieceMove.java
@@ -16,4 +16,15 @@ public class PieceMove {
         this.piecePosition = piecePosition;
         this.targetSquare = targetSquare;
     }
+
+    @Override
+    public String toString() {
+        return positionToNotation(piecePosition)+positionToNotation(targetSquare);
+    }
+
+    private String positionToNotation(byte position) {
+        String row = String.valueOf((position/8)+1);
+        char colum = (char) ((7-(position%8))+'a');
+        return colum+row;
+    }
 }
